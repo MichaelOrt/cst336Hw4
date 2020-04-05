a0064a0f4d55046611ad8a94020020fd7329b268
Michael <micortega@csumb.edu>
2020-04-04 18:47:12 -0700

    hw finished

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const faker = require('faker')
+const app = express();
+const port = 3000
+
+
+app.use(express.static("public"));
+
+app.engine('html', require('ejs').renderFile);
+
+//routes
+app.get("/", function(req, res){
+     res.render('index.ejs');
+});
+app.get("/attacks", function(req, res){
+     res.render('attacks.ejs');
+});
+app.get("/examples", function(req, res){
+     res.render('examples.ejs');
+});
+app.get("/prevention", function(req, res){
+	res.render('prevention.ejs');
+});
+app.get("/faker", function(req, res){
+	var info = [7];
+	info[0] = faker.name.findName();
+	info[1] = faker.image.imageUrl();
+	info[2] = faker.address.city();
+	info[3] = faker.address.zipCode();
+	info[4] = faker.address.streetAddress();
+	info[5] = faker.address.county();
+	info[6] = faker.address.country();
+	res.render('faker.ejs',{'info': info});
+});
+//process.env.PORT, process.env.IP
+app.listen(port, function(){
+   console.log("Running Express Server..."); 
+});
